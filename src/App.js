f8a727ca9197a448b310e229df72347e1d8955f0
amitX241 <2022sp93059@wilp.bits-pilani.ac.in>
2023-04-07 12:39:56 +0530

    Jenkins Trigger v3

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload the app successfully.
+          Edit <code>src/App.js</code> and save to reload the app successfully and run.
         </p>
         <a
           className="App-link"
